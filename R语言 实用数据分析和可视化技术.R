1be14830078e5e55e93f00665951b83bf3b2f47c
Stella Gao <stella.gao.89@gmail.com>
2016-08-06 15:32:31 -0500

    Update R语言 实用数据分析和可视化技术.R

--- R语言 实用数据分析和可视化技术.R
+++ R语言 实用数据分析和可视化技术.R
@@ -6,4 +6,26 @@
 > assign("j",4)
 > j
 [1] 4
-> 
+> rm(j)   # 删除变量
+> j
+Error: object 'j' not found
+> class(a)  # 查看变量中存储的数据类型
+[1] "numeric"
+> is.numeric(b)  # 检验一个变量是否是数值型
+[1] TRUE
+> i<-5L
+> i
+[1] 5
+> is.integer(i)
+[1] TRUE
+> x<-"data"
+> x
+[1] "data"
+> y<-factor("data")
+> y
+[1] data
+Levels: data
+> nchar(x)   # 得到字符的长度
+[1] 4
+> nchar(y)
+Error in nchar(y) : 'nchar()' requires a character vector
